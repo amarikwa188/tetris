e49b189f0cb2299e9e4cfb50f4a97b8358fb78f8
Amarikwa <amarikwa188@gmail.com>
2024-09-06 10:19:17 +0100

    created game settings

---
+++ game_settings.py
@@ -0,0 +1,3 @@
+# screen dimensions
+SCREEN_WIDTH: int = 400
+SCREEN_HEIGHT: int = 800
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 from pygame import Surface
 
+import game_settings as s
+
 
 def run_game() -> None:
     pygame.init()
     pygame.display.set_caption("Tetris")
+
+    screen: Surface = pygame.display.set_mode((s.SCREEN_WIDTH,
+                                               s.SCREEN_HEIGHT))
+    
+    while True:
+        pass
 
 
 if __name__ == "__main__":

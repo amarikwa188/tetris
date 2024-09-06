ef72736f366e158ab46ec6f984b366656976786e
Amarikwa <amarikwa188@gmail.com>
2024-09-06 13:27:05 +0100

    rearranged code structure

--- game_settings.py
+++ game_settings.py
@@ -1,21 +1,4 @@
-######### IMPORTS ##########
-from typing import Any
+screen_width: int = 500
+screen_height: int = 500
 
-import pygame
-from pygame import Surface
-
-from states.main_menu import MainMenu
-############################
-
-# screen dimensions
-SCREEN_WIDTH: int = 500
-SCREEN_HEIGHT: int = 500
-
-# screen
-screen: Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# track game state
-current_state: Any = MainMenu(screen)
-
-# framerate
-FRAMERATE: int = 60
+framerate: int = 60
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+import sys
+from typing import Any
+
 import pygame
+from pygame import Surface
 from pygame.time import Clock
 
 import game_settings as gs
-import game_functions as gf
 
 
-def run_game() -> None:
-    pygame.init()
-    pygame.display.set_caption("Tetris")
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen: Surface = pygame.display.set_mode((gs.screen_width,
+                                                        gs.screen_height))
+        self.clock: Clock = pygame.time.Clock()
 
-    clock: Clock = pygame.time.Clock()
+        self.current_state: Any | None = None
 
-    while True:
-        clock.tick(gs.FRAMERATE)
-        gf.check_events()
-        gs.current_state.run()
-        gf.update_screen()
+
+    def run(self) -> None:
+        while True:
+            self.clock.tick(gs.framerate)
+            self.check_events()
+            pygame.display.update()
+
+
+    def check_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 
 if __name__ == "__main__":
-    run_game()
+    game: Game = Game()
+    game.run()

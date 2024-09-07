1f46cc7948f3a7dffe105b2cf119ff16b6057a87
Amarikwa <amarikwa188@gmail.com>
2024-09-07 13:01:22 +0100

    create a tetrinome and draw it to the screen

--- states/main_game.py
+++ states/main_game.py
@@ -12,13 +12,23 @@ class Tetris:
         self.screen: Surface = screen
         self.state_manager: StateManager =  state_manager
 
+        self.block_group: Group = Group()
+        self.tetromino: Tetronimo = Tetronimo(self.block_group)
+
 
     def run(self) -> None:
         self.screen.fill((230,230,230))
         self.draw_grid()
 
+        self.tetromino.update()
+
+        self.block_group.update()
+        self.block_group.draw(self.screen)
+
+
     def handle_events(self, event: Event) -> None:
         pass
+
 
     def draw_grid(self) -> None:
         for col in range(gs.grid_width):
@@ -26,17 +36,30 @@ class Tetris:
                 col_pos: int = col * gs.tile_size + gs.grid_start_x
                 row_pos: int = row * gs.tile_size + gs.grid_start_y
 
-                # print(col_pos, row_pos) # (x,y) co-ordinates
-
                 pygame.draw.rect(self.screen, (0,0,0),
                                  (col_pos, row_pos, gs.tile_size, gs.tile_size),
                                   1)
 
 class Tetronimo:
-    def __init__(self) -> None:
+    def __init__(self, block_group: Group) -> None:
+        self.block_group: Group = block_group
+        
+        self.shape: str = 'T'
+        self.block: Block = [Block(pos, self.block_group) 
+                             for pos in gs.TETROMINOES[self.shape]]
+
+    def update(self) -> None:
         pass
 
 
 class Block(Sprite):
-    def __init__(self, tetronimo: Tetronimo, position: tuple[int,int]) -> None:
+    def __init__(self, position: tuple[int,int], block_group: Group) -> None:
         super().__init__()
+        block_group.add(self)
+
+        self.image: Surface = Surface((gs.tile_size, gs.tile_size))
+        self.image.fill("red")
+
+        self.rect: Rect = self.image.get_rect()
+        self.rect.topleft = position[0] * gs.tile_size + gs.grid_start_x, \
+                            position[1] * gs.tile_size + gs.grid_start_y
